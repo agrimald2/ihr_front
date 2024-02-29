6960f3cfbfc355be91a4a6614b69e353ef7588d5
Marco Corbera <marco.corbera@zaelot.com>
2024-02-29 16:36:54 -0500

    Clean package.json nuxt config and rm package-lock

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,6 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  runtimeConfig: {
-    // Private keys are only available on the server
-    apiSecret: '123',
-
-    // Public keys that are exposed to the client
-    public: {
-      apiBase: process.env.BASE_URL || '/api'
-    }
-  },
   modules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/i18n'
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss'
   ],
-  i18n: {
-    vueI18n: './i18n.config.ts',
-  }
 })
