04242616878cfa859da74073ffe54d52ecb72bba
agrimal2 <agrimaldopci18@gmail.com>
2024-03-06 20:47:43 -0500

    FormUpdate and Switcher

--- components/checkout/PaymentMethod.vue
+++ components/checkout/PaymentMethod.vue
@@ -5,34 +5,41 @@
         <label v-for="{ label, value, logo, active } in paymentMethods" :key="value" class="relative">
           <input type="radio" class="peer sr-only" :disabled="!active" />
           <div
-            class="h-1/3 flex flex-col items-center justify-center py-7 px-4 cursor-pointer rounded-md border border-neutral-200 -outline-offset-2 hover:border-primary-200 hover:bg-primary-100 peer-focus:border-primary-200 peer-focus:bg-primary-100 active:border-primary-300 active:bg-primary-200 peer-checked:outline peer-checked:outline-2 peer-checked:outline-primary-700 peer-disabled:opacity-50 peer-disabled:bg-neutral-100 peer-disabled:border-neutral-200 peer-disabled:cursor-not-allowed peer-disabled:[&_img]:grayscale"
-          >
+            class="h-1/3 flex flex-col items-center justify-center py-7 px-4 cursor-pointer rounded-md border border-neutral-200 -outline-offset-2 hover:border-primary-200 hover:bg-primary-100 peer-focus:border-primary-200 peer-focus:bg-primary-100 active:border-primary-300 active:bg-primary-200 peer-checked:outline peer-checked:outline-2 peer-checked:outline-primary-700 peer-disabled:opacity-50 peer-disabled:bg-neutral-100 peer-disabled:border-neutral-200 peer-disabled:cursor-not-allowed peer-disabled:[&_img]:grayscale">
             <img :src="logo" :alt="label" class="h-6 select-none" />
-            <p v-if="!active" class="absolute bottom-2 select-none text-disabled-900 typography-text-xs mt">Coming soon</p>
+            <p v-if="!active" class="absolute bottom-2 select-none text-disabled-900 typography-text-xs mt">Coming soon
+            </p>
           </div>
         </label>
       </div>
     </fieldset>
-    <form class="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div class="col-span-2 grid-cols-1 flex-col flex w-1/2">
-        <label class="typography-text-sm font-medium">Name on card</label>
-        <input v-model="nameOnCard" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Nombre del titular">
-      </div>
-      <div class="grid-cols-2 flex-col flex w-auto">
-        <label class="typography-text-sm font-medium">Card Number</label>
-        <input v-model="cardNumber" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cardNumber" type="text" placeholder="Numero de la tarjeta">
-      </div>
-      <div class="grid-cols-1 flex-col flex w-auto">
-        <label class="typography-text-sm font-medium">Expiration Month</label>
-        <input v-model="expirationMonth" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="expiracyMonth" type="text" placeholder="Mes de expiración">
-      </div>
-      <div class="grid-cols-1 flex-col flex w-auto">
-        <label class="typography-text-sm font-medium">Expiration Year</label>
-        <input v-model="expirationYear" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="expiracyYear" type="text" placeholder="Año de expiración" name="expiracyYear" required />
-      </div>
-      <div class="grid-cols-1 flex-col flex w-auto">
-        <label class="typography-text-sm font-medium">Security Code</label>
-        <input v-model="securityCode" class="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="securityCode" type="text" placeholder="CVV" name="securityCode" required />
+    <form>
+      <div class="grid grid-cols-6 gap-3">
+        <div class="col-span-6 sm:col-span-6">
+          <label for="card-number" class="block text-sm font-medium text-gray-700 mb-2">Card Number</label>
+          <input v-model="cardNumber" type="text" name="card-number" id="card-number" placeholder="0000 0000 0000 0000"
+            class="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500">
+        </div>
+        <div class="col-span-2 sm:col-span-2">
+          <label for="expiration-month" class="block text-sm font-medium text-gray-700 mb-2">Month</label>
+          <input v-model="expirationMonth" type="text" name="expiration-month" id="expiration-month" placeholder="00"
+            class="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500">
+        </div>
+        <div class="col-span-2 sm:col-span-2">
+          <label for="expiration-date" class="block text-sm font-medium text-gray-700 mb-2">Year</label>
+          <input v-model="expirationYear" type="text" name="expiration-date" id="expiration-date" placeholder="0000"
+            class="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500">
+        </div>
+        <div class="col-span-2 sm:col-span-2">
+          <label for="cvv" class="block text-sm font-medium text-gray-700 mb-2"> Code</label>
+          <input v-model="securityCode" type="text" name="cvv" id="cvv" placeholder="000"
+            class="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500">
+        </div>
+        <div class="col-span-6 sm:col-span-6">
+          <label for="card-holder" class="block text-sm font-medium text-gray-700 mb-2">Card Holder</label>
+          <input v-model="nameOnCard" type="text" name="card-holder" id="card-holder" placeholder="Full Name"
+            class="w-full py-3 px-4 border border-gray-400 rounded-lg focus:outline-none focus:border-blue-500">
+        </div>
       </div>
     </form>
   </div>
@@ -71,16 +78,16 @@ const paymentMethods = [
     active: true,
   },
   {
-    label: 'PayPal',
-    value: 'paypal',
-    logo: 'https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/paypal-logo.svg',
-    active: false,
+    label: 'Crypto',
+    value: 'crypto',
+    logo: 'https://pngimg.com/d/bitcoin_PNG12.png',
+    active: true,
   },
   {
     label: 'ApplePay',
     value: 'applepay',
     logo: 'https://storage.googleapis.com/sfui_docs_artifacts_bucket_public/production/apple-pay-logo.svg',
-    active: false,
+    active: true,
   },
   {
     label: 'GooglePay',
--- components/checkout/Review.vue
+++ components/checkout/Review.vue
@@ -1,9 +1,26 @@
 <template>
-<div class="px-4 max-w-xl">
-  <a class="block max-w-xl p-6 rounded-lg shadow">
-    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Shipping and Payment</h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-  </a>
-</div>
-
+  <div class="px-4 max-w-xl">
+    <a class="block max-w-xl p-6 rounded-lg shadow">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Shipping and Payment</h5>
+      <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of
+        2021 so far, in reverse chronological order.</p>
+    </a>
+    <div class="mt-4 p-4 rounded-lg shadow-lg bg-blue-100">
+      <p class="text-lg font-semibold">Powered by {{ paymentGateway }}...</p>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const paymentGateways = ['MercadoPago', 'Openpay'];
+const paymentGateway = ref('');
+
+function selectRandomGateway() {
+  const index = Math.floor(Math.random() * paymentGateways.length);
+  paymentGateway.value = paymentGateways[index];
+}
+
+selectRandomGateway();
+</script>
